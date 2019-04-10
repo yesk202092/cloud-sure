19bb6c1e83634922df56563a2ec23373600b8d43
yesk <ysk202092@163.com>
2019-04-10 22:35:19 +0800

    update:中转首页

--- sure-admin/src/main/java/sure/AdminBootStart.java
+++ sure-admin/src/main/java/sure/AdminBootStart.java
@@ -1,18 +1,11 @@
 package sure;
 
-import com.sun.deploy.net.HttpResponse;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @Controller
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
@@ -20,7 +13,7 @@ import java.io.IOException;
 public class AdminBootStart {
 
     @RequestMapping(value = "/")
-    public String  index(HttpServletResponse response, HttpServletRequest request) throws IOException {
+    public String  index() {
         return "redirect:index";
     }
     public static void main(String[] args) {
