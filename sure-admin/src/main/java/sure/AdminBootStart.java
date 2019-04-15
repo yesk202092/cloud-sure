ade3ba8e074beca3ab424be52a5f646d95890ca4
yesk <ysk202092@163.com>
2019-04-15 17:59:18 +0800

    update:增加配置中心

--- sure-admin/src/main/java/sure/AdminBootStart.java
+++ sure-admin/src/main/java/sure/AdminBootStart.java
@@ -1,5 +1,6 @@
 package sure;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
--- sure-boot/config/src/main/java/sure/ConfigBootStart.java
+++ sure-boot/config/src/main/java/sure/ConfigBootStart.java
@@ -1,7 +1,16 @@
 package sure;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@EnableConfigServer
+@EnableEurekaClient
 public class ConfigBootStart {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(ConfigBootStart.class).web(true).run(args);
+    }
 }
